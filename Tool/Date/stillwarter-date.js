15edeab097d55f270c76b585fd342b91f2c1f005
stillwarter <2406017193@qq.com>
2024-01-10 11:23:17 +0800

    Add:time function

--- Tool/Date/stillwarter-date.js
+++ Tool/Date/stillwarter-date.js
@@ -39,3 +39,23 @@ function getdateym() {
 function getdatedayname() {
   return "7123456".charAt(new Date().getDay());
 }
+
+
+/* 时间戳转换为时间 */
+export function timestampToTime(timestamp) {
+  timestamp = timestamp ? timestamp : null;
+  let date = new Date(timestamp); //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+  let Y = date.getFullYear() + "-";
+  let M =
+    (date.getMonth() + 1 < 10
+      ? "0" + (date.getMonth() + 1)
+      : date.getMonth() + 1) + "-";
+  let D = (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + " ";
+  let h =
+    (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":";
+  let m =
+    (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) +
+    ":";
+  let s = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+  return Y + M + D + h + m + s;
+}
